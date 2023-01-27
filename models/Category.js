12847537ae23a6518caa4b9753f8ca7608fe8058
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-01-27 08:43:47 -0600

    Added sudocode

--- models/Category.js
+++ models/Category.js
@@ -6,12 +6,14 @@ class Category extends Model {}
 
 Category.init(
   {
+    // Integer | Doesn't allow null values | Set as primary key | Uses auto increment
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
+    // String | Doesn't allow null values
     category_name: {
       type: DataTypes.STRING,
       allowNull: false
